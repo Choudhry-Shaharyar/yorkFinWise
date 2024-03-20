0d0bae2a6fb484edf85edb762d094bf7b5b8df56
Choudhry-Shaharyar <sheary009@hotmail.com>
2024-03-19 21:43:06 -0400

    removed image

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -6,98 +6,98 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const Jobs = () => {
   const scholarships = [
-    { 
-      title: "President’s International Scholarship of Excellence", 
-      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=4849", 
+    {
+      title: "President’s International Scholarship of Excellence",
+      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=4849",
       description: "Twenty (20) of these scholarships are awarded per year, valued at $45,000/year. It is renewable for an additional three years of full-time undergraduate study if the student maintains good academic standing each year, bringing it to a total of $180,000 over four years.",
       amount: "$180,000 ($45,000 x 4 years)"
     },
-    { 
-      title: "Tentanda Via Award", 
-      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=4881", 
+    {
+      title: "Tentanda Via Award",
+      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=4881",
       description: "Fifteen (15) of these awards are awarded per year, valued at $30,000/year. It is renewable for an additional three years of full-time undergraduate study if the student maintains good academic standing each year, bringing it to a total of $120,000 over four years.",
       amount: "$120,000 ($30,000 x 4 years)"
     },
-    { 
-      title: "Global Leader of Tomorrow Award for International Students", 
-      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=0966", 
+    {
+      title: "Global Leader of Tomorrow Award for International Students",
+      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=0966",
       description: "Two (2) of these scholarships are awarded per year, valued at $20,000/year. It is renewable for an additional three years of full-time undergraduate study if the student maintains high academic standing each year, bringing it to a total of $80,000 over four years.",
       amount: "$80,000 ($20,000 x 4 years)"
     },
-    { 
-      title: "International Circle of Scholar’s Scholarship", 
-      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=0977", 
+    {
+      title: "International Circle of Scholar’s Scholarship",
+      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=0977",
       description: "One (1) of these scholarships is awarded per year, valued at $15,000. It is for $15,000 towards a student’s first year of study.",
       amount: "$15,000"
     },
-    { 
-      title: "York University Academic Excellence Scholarships for International Students", 
-      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=3797", 
+    {
+      title: "York University Academic Excellence Scholarships for International Students",
+      link: "https://sfs.yorku.ca/scholarships/award-search?awardID=3797",
       description: "Five (5) to 10 of these scholarships are awarded per year, valued at $5,000 to $10,000. It is for $5,000 to $10,000 towards a student’s first year of study.",
       amount: "$5,000-10,000"
     },
-    { 
-      title: 'Liberal Arts & Professional Studies International Undergraduate Leadership Scholarships', 
-      link: 'https://www.yorku.ca/laps/students/international/awards/', 
-      description: '($10,000 x 4 years)\nFor Liberal Arts & Professional Studies applicants and requires an application.\nApplication Deadline: TBD\nSubmit the Liberal Arts & Professional Studies International Students Scholarship application.', 
-      amount: '$40,000' 
+    {
+      title: 'Liberal Arts & Professional Studies International Undergraduate Leadership Scholarships',
+      link: 'https://www.yorku.ca/laps/students/international/awards/',
+      description: '($10,000 x 4 years)\nFor Liberal Arts & Professional Studies applicants and requires an application.\nApplication Deadline: TBD\nSubmit the Liberal Arts & Professional Studies International Students Scholarship application.',
+      amount: '$40,000'
     },
-    { 
-      title: 'Liberal Arts & Professional Studies International Student Entrance Scholarship', 
-      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=3861', 
-      description: 'For Liberal Arts & Professional Studies applicants\nNo award application required', 
-      amount: '$2,000' 
+    {
+      title: 'Liberal Arts & Professional Studies International Student Entrance Scholarship',
+      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=3861',
+      description: 'For Liberal Arts & Professional Studies applicants\nNo award application required',
+      amount: '$2,000'
     },
-    { 
-      title: 'Faculty of Health International Student Entrance Scholarship of Excellence', 
-      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=5121', 
-      description: 'For Faculty of Health applicants\nNo award application required', 
-      amount: '$2,000' 
+    {
+      title: 'Faculty of Health International Student Entrance Scholarship of Excellence',
+      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=5121',
+      description: 'For Faculty of Health applicants\nNo award application required',
+      amount: '$2,000'
     },
-    { 
-      title: 'Glendon International Excellence Scholarship', 
-      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=4887', 
-      description: 'For Glendon applicants and requires an application.\nApplication Deadline: February 15\nSubmit the Glendon International Students Scholarship application package.', 
-      amount: '$40,000\n($10,000 x 4 years)' 
+    {
+      title: 'Glendon International Excellence Scholarship',
+      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=4887',
+      description: 'For Glendon applicants and requires an application.\nApplication Deadline: February 15\nSubmit the Glendon International Students Scholarship application package.',
+      amount: '$40,000\n($10,000 x 4 years)'
     },
-    { 
-      title: 'Glendon International Scholarship', 
-      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=4537', 
-      description: 'For Glendon applicants and requires an application.\nApplication Deadline: February 15\nSubmit the Glendon International Students Scholarship application package.', 
-      amount: '$5,000-$10,000' 
+    {
+      title: 'Glendon International Scholarship',
+      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=4537',
+      description: 'For Glendon applicants and requires an application.\nApplication Deadline: February 15\nSubmit the Glendon International Students Scholarship application package.',
+      amount: '$5,000-$10,000'
     },
-    { 
-      title: 'Faculty of Science Entrance Scholarship', 
-      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=3941', 
-      description: 'For Faculty of Science applicants\nNo award application required', 
-      amount: '$2,000' 
+    {
+      title: 'Faculty of Science Entrance Scholarship',
+      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=3941',
+      description: 'For Faculty of Science applicants\nNo award application required',
+      amount: '$2,000'
     },
-    { 
-      title: 'York Science Scholars Award', 
-      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=4429', 
-      description: 'For Faculty of Science applicants and requires an application\n$5,000 as an entrance scholarship + $5,000 as a guaranteed summer research position following the first year of study\nApplication Deadline: TBD\nFor more information - York Science Scholars Award (YSSA)', 
-      amount: '$10,000' 
+    {
+      title: 'York Science Scholars Award',
+      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=4429',
+      description: 'For Faculty of Science applicants and requires an application\n$5,000 as an entrance scholarship + $5,000 as a guaranteed summer research position following the first year of study\nApplication Deadline: TBD\nFor more information - York Science Scholars Award (YSSA)',
+      amount: '$10,000'
     },
-    { 
-      title: 'The Annie Demirjian’77 Scholarship', 
-      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=4566', 
-      description: 'For Glendon applicants and requires an application.\nApplication Deadline: February 15\nSubmit the Glendon International Students Scholarship application package.', 
-      amount: '$5,000-10,000' 
+    {
+      title: 'The Annie Demirjian’77 Scholarship',
+      link: 'https://sfs.yorku.ca/scholarships/award-search?awardID=4566',
+      description: 'For Glendon applicants and requires an application.\nApplication Deadline: February 15\nSubmit the Glendon International Students Scholarship application package.',
+      amount: '$5,000-10,000'
     },
-    { 
-      title: 'York University Talent Entrance Scholarships', 
-      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=0066', 
-      description: 'For applicants to programs in the School of the Arts, Media, Performance & Design (AMPD)\nNo award application required', 
-      amount: '$1,000' 
+    {
+      title: 'York University Talent Entrance Scholarships',
+      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=0066',
+      description: 'For applicants to programs in the School of the Arts, Media, Performance & Design (AMPD)\nNo award application required',
+      amount: '$1,000'
     },
-    { 
-      title: 'Lassonde Entrance Scholarships', 
-      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=3940', 
-      description: 'For Lassonde school of Engineering applicants\nNo award application required', 
-      amount: '$2,000' 
+    {
+      title: 'Lassonde Entrance Scholarships',
+      link: 'http://sfs.yorku.ca/scholarships/award-search?awardID=3940',
+      description: 'For Lassonde school of Engineering applicants\nNo award application required',
+      amount: '$2,000'
     }
   ];
-  
+
 
   function SamplePrevArrow(props) {
     const { className, style, onClick } = props;
@@ -215,17 +215,17 @@ const Jobs = () => {
         <div className="jobtitle">
           <h1>Scholarships</h1>
           <p>
-          York University offers a variety of scholarships and awards that recognize academic excellence, resilience and community leadership. Some financial awards require that you submit an application for consideration, while other entrance awards are automatically assessed and awarded at the time of your admission.
+            York University offers a variety of scholarships and awards that recognize academic excellence, resilience and community leadership. Some financial awards require that you submit an application for consideration, while other entrance awards are automatically assessed and awarded at the time of your admission.
           </p>
         </div>
         <div className="space3"></div>
 
         <div>
-         < ScholarshipsTable scholarships={scholarships} />
+          < ScholarshipsTable scholarships={scholarships} />
         </div>
 
-       
-  
+
+
 
 
         <div className="space"></div>
@@ -233,49 +233,47 @@ const Jobs = () => {
         <div className="jobtitle">
           <h1>Tips</h1>
         </div>
-      <div className="tips-container">
-        <div className="tips">
-      <h2>Saving Money as a York University Student</h2>
-      <div className="tip">
-        <h3>Budegt Calculator:</h3>
-        <p>
-         Use the RBC Student Budget Calculator* to help you figure out how much money you'll need to get through the school year. When you don't have an exact amount to work with, estimate on the high side. You can always go back and rework the figures later. Access the RBC Student Budget Calculator <a href="https://www.rbcroyalbank.com/student/budget-calculator/">here</a>.
-        </p>
-      </div>
-      <div className="tip">
-        <h3>Food Savings:</h3>
-        <p>
-  Explore the benefits of York University Mobile Ordering: Begin placing orders and earn 10 points for every dollar you spend! This user-friendly app not only simplifies your food orders but also aids in saving money on every purchase. Additionally, keep an eye on time-sensitive specials in our dining halls by following <a href="https://www.instagram.com/yorkufood/">@yorkufood</a> on Instagram! Learn more about the rewards program <a href="https://www.yorku.ca/foodservices/mobileapp/#rewards">here</a>.
-</p>
+        <div className="tips-container">
+          <div className="tips">
+            <h2>Saving Money as a York University Student</h2>
+            <div className="tip">
+              <h3>Budegt Calculator:</h3>
+              <p>
+                Use the RBC Student Budget Calculator* to help you figure out how much money you'll need to get through the school year. When you don't have an exact amount to work with, estimate on the high side. You can always go back and rework the figures later. Access the RBC Student Budget Calculator <a href="https://www.rbcroyalbank.com/student/budget-calculator/">here</a>.
+              </p>
+            </div>
+            <div className="tip">
+              <h3>Food Savings:</h3>
+              <p>
+                Explore the benefits of York University Mobile Ordering: Begin placing orders and earn 10 points for every dollar you spend! This user-friendly app not only simplifies your food orders but also aids in saving money on every purchase. Additionally, keep an eye on time-sensitive specials in our dining halls by following <a href="https://www.instagram.com/yorkufood/">@yorkufood</a> on Instagram! Learn more about the rewards program <a href="https://www.yorku.ca/foodservices/mobileapp/#rewards">here</a>.
+              </p>
 
+            </div>
+            <div className="tip">
+              <h3>Textbook Savings:</h3>
+              <p>Textbook Guaranteed Buyback is another fantastic way to save with York University Bookstore. Here's how it works: Check our Guaranteed Buyback List or the shelf tag for your new or used textbooks for the Fall/Winter semester. Keep studying hard and delve into those books. Then, bring your book(s) in good condition, and we will guarantee to pay you back the exact amount! Learn more about our Guaranteed Buyback program <a href="https://www.bookstore.yorku.ca/buyback.asp">here</a>.
+              </p>
+            </div>
+            <div className="tip">
+              <h3>Grocery Savings:</h3>
+              <p>Students can find affordable groceries at the nearby Walmart, offering a wide range of products at competitive prices. Shopping on student discount days at local supermarkets, joining food co-ops, and participating in bulk buying groups can further reduce grocery expenses. Additionally, consider checking out <a href="https://regenesis.eco/en/yumarket">Yumarket</a> for more options.</p>
+            </div>
+            <div className="tip">
+              <h3>Transportation Savings:</h3>
+              <p>Save money on transportation by utilizing public transportation or considering biking or walking to campus. Take advantage of student discounts such as the PRESTO on GO, which offers up to 40%* off fares. For more information, visit <a href="https://www.gotransit.com/en/student-savings/post-secondary-discount" target="_blank" rel="noopener noreferrer">GO Transit - Post-Secondary Discount</a>.</p>
+            </div>
+            <div className="tip">
+              <h3>YFS Entertainment Savings:</h3>
+              <p>Explore the YFS Online Discounts page for exclusive deals and codes that help you save money on various products and services. From tech gadgets to clothing and food delivery, you'll find a wide range of offers tailored to student needs. Don't miss out on these savings!
+                For additional discounts and online codes, check out <a href="https://www.yfs.ca/online-discounts">YFS Online Discounts</a>.</p>
+            </div>
+          </div>
+          <div className="money"></div>
+        </div>
       </div>
-      <div className="tip">
-        <h3>Textbook Savings:</h3>
-        <p>Textbook Guaranteed Buyback is another fantastic way to save with York University Bookstore. Here's how it works: Check our Guaranteed Buyback List or the shelf tag for your new or used textbooks for the Fall/Winter semester. Keep studying hard and delve into those books. Then, bring your book(s) in good condition, and we will guarantee to pay you back the exact amount! Learn more about our Guaranteed Buyback program <a href="https://www.bookstore.yorku.ca/buyback.asp">here</a>.
-</p>
-      </div>
-      <div className="tip">
-        <h3>Grocery Savings:</h3>
-        <p>Students can find affordable groceries at the nearby Walmart, offering a wide range of products at competitive prices. Shopping on student discount days at local supermarkets, joining food co-ops, and participating in bulk buying groups can further reduce grocery expenses. Additionally, consider checking out <a href="https://regenesis.eco/en/yumarket">Yumarket</a> for more options.</p>
-      </div>
-      <div className="tip">
-        <h3>Transportation Savings:</h3>
-        <p>Save money on transportation by utilizing public transportation or considering biking or walking to campus. Take advantage of student discounts such as the PRESTO on GO, which offers up to 40%* off fares. For more information, visit <a href="https://www.gotransit.com/en/student-savings/post-secondary-discount" target="_blank" rel="noopener noreferrer">GO Transit - Post-Secondary Discount</a>.</p>
-      </div>
-      <div className="tip">
-        <h3>YFS Entertainment Savings:</h3>
-        <p>Explore the YFS Online Discounts page for exclusive deals and codes that help you save money on various products and services. From tech gadgets to clothing and food delivery, you'll find a wide range of offers tailored to student needs. Don't miss out on these savings!
- For additional discounts and online codes, check out <a href="https://www.yfs.ca/online-discounts">YFS Online Discounts</a>.</p>
-      </div>
-    </div>
-      <div className="money"></div>
-    </div>
-    </div>
+      <div className="space"></div>
 
 
-        <div className="space"></div>
-
-        
 
 
 
